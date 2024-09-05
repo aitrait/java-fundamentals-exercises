aec40c5321d721181aa7f2625d43d1fd9a1ab309
5hv3ts0v <olexii.shvetsov@stud.onu.edu.ua>
2024-09-05 09:16:37 +0300

    Update ExerciseIntroduction.java
    complete

--- 0-0-intro/src/main/java/com/bobocode/intro/ExerciseIntroduction.java
+++ 0-0-intro/src/main/java/com/bobocode/intro/ExerciseIntroduction.java
@@ -1,6 +1,6 @@
 package com.bobocode.intro;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.util.Base64;
 
 /**
  * Welcome! This is an introduction exercise that will show you a simple example of Bobocode exercises.
@@ -8,7 +8,6 @@ import com.bobocode.util.ExerciseNotCompletedException;
  * JavaDoc is a way of communication with other devs. We use Java Docs in every exercise to define the task.
  * So PLEASE MAKE SURE you read the Java Docs carefully.
  * <p>
- * Every exercise is covered with tests, see {@link ExerciseIntroductionTest}.
  * <p>
  * In this repo you'll find dozens of exercises covering various fundamental topics.
  * They all have the same structure helping you to focus on practice and build strong skills!
@@ -24,22 +23,10 @@ public class ExerciseIntroduction {
      */
     public String getWelcomeMessage() {
         // todo: implement a method and return a message according to javadoc
-        throw new ExerciseNotCompletedException(); 
+        return "The key to efficient learning is practice!";
     }
 
-    /**
-     * Method encodeMessage accepts one {@link String} parameter and returns encoded {@link String}.
-     * <p>
-     * PLEASE NOTE THAT YOU WILL GET STUCK ON THIS METHOD INTENTIONALLY! ;)
-     * <p>
-     * Every exercise has a completed solution that is stored in the branch "completed". So in case you got stuck
-     * and don't know what to do, go check out completed solution.
-     *
-     * @param message input message
-     * @return encoded message
-     */
     public String encodeMessage(String message) {
-        // todo: switch to branch "completed" in order to see how it should be implemented
-        throw new ExerciseNotCompletedException();
+        return Base64.getEncoder().encodeToString(message.getBytes());
     }
 }
